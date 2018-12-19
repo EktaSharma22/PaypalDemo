515ec5f42e41ad83d6b9bee7013efa35559de89e
ganesha <testergws@gmail.com>
2018-12-19 10:27:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ganesha/demobookmytable/Config/Config.java
@@ -0,0 +1,6 @@
+package com.example.ganesha.demobookmytable.Config;
+
+public class Config
+{
+    public static final String PAYPAL_CLIENT_ID = "AeI85V_zVE8EHxx8dwoEdHFh6qiW_8eR4l5N_Dgfe7AsD_v802_LSsP5l_ZPU8SAAfZxY2q3m-irewLc";
+}
